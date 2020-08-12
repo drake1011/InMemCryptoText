956b9bb1464ccdc66e6931cd522510893857d291
drake-vcu <dkaempfer46@gmail.com>
2020-08-11 19:53:14 -0500

    Initial commit

---
+++ CryptoLib/Cryptography.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoLib
+{
+    public static class Cryptography
+    {
+        #region Settings
+
+        // If these setting change, previous encrypted files won't be able to decrypt using new setting if they were encrypted with old settings
+
+        private static int _iterations = 7;
+        private static int _keySize = 256;
+
+        private static string _hash = "SHA1";
+        private static string _salt = "gseRrias31490b32"; // Random
+        private static string _vector = "1873az34rTl34kjq"; // Random
+
+        #endregion
+
+        /// <summary>
+        /// Encrypt using AesManaged implementation of AES
+        /// </summary>
+        /// <param name="value">value to encrypt</param>
+        /// <param name="password">password to use for encryption and decription</param>
+        /// <returns>Encrypted data as string</returns>
+        public static string Encrypt(string value, string password)
+        {
+            return Encrypt<AesManaged>(value, password);
+        }
+
+        /// <summary>
+        /// Encrypt using SymetricAlgorithm Cipher of choice
+        /// </summary>
+        /// <typeparam name="T">Implementation of SymetricAlgorithm (of choice)</typeparam>
+        /// <param name="value">value to encrypt</param>
+        /// <param name="password">password to use for encryption and decription</param>
+        /// <returns>Encrypted data as string</returns>
+        public static string Encrypt<T>(string value, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = UTF8Encoding.UTF8.GetBytes(value);
+
+            byte[] encrypted;
+            using (T cipher = new T())
+            {
+                var _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
+                {
+                    using (var memStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            cryptoStream.Write(valueBytes, 0, valueBytes.Length);
+                            cryptoStream.FlushFinalBlock();
+                            encrypted = memStream.ToArray();
+                        }
+                    }
+                }
+                cipher.Clear();
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+
+        /// <summary>
+        /// Decrypt using AesManaged implementation of AES
+        /// </summary>
+        /// <param name="value">value to encrypt</param>
+        /// <param name="password">password to use for encryption and decription</param>
+        /// <returns>Decrypted plain text as string</returns>
+        public static string Decrypt(string value, string password)
+        {
+            return Decrypt<AesManaged>(value, password);
+        }
+
+        /// <summary>
+        /// Decrypt using SymetricAlgorithm Cipher of choice
+        /// </summary>
+        /// <typeparam name="T">Implementation of SymetricAlgorithm (of choice)</typeparam>
+        /// <param name="value">value to encrypt</param>
+        /// <param name="password">password to use for encryption and decription</param>
+        /// <returns>Decrypted plain text as string</returns>
+        public static string Decrypt<T>(string value, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = Convert.FromBase64String(value);
+
+            byte[] decrypted;
+            int decryptedByteCount = 0;
+
+            using (T cipher = new T())
+            {
+                var _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                try
+                {
+                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
+                    {
+                        using (var memStream = new MemoryStream(valueBytes))
+                        {
+                            using (var cryptoStream = new CryptoStream(memStream, decryptor, CryptoStreamMode.Read))
+                            {
+                                decrypted = new byte[valueBytes.Length];
+                                decryptedByteCount = cryptoStream.Read(decrypted, 0, decrypted.Length);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // TODO proper exception handling
+                    // TODO when password is bad, instead of returning empty string - throw an exception?
+                    return string.Empty;
+                }
+
+                cipher.Clear();
+            }
+            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
+        }
+    }
+}
---
+++ XUnitTests/CryptoLibTests.cs
@@ -0,0 +1,65 @@
+﻿using CryptoLib;
+using System;
+using System.Text;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class CryptoLibTests
+    {
+        #region Cryptography
+
+        [Fact]
+        public void Cryptography_aes_encrypt_and_decrypt_match_Pass()
+        {
+            var resultString = Cryptography.Encrypt("my secret", "myPass");
+            var decriptedResultString = Cryptography.Decrypt(resultString, "myPass");
+            Assert.Equal("my secret", decriptedResultString);
+        }
+
+        [Fact]
+        public void Cryptography_aes_encrypt_and_decrypt_match_long_value_Pass()
+        {
+            const string value = "my secret super long value that isnt very short it is actually really really really, really really 01234566, really really long" +
+                "almost too long tbg.\n This is not the end of this long value. 1234567012349412349 1234921349213 1234923942139 really 123049321940194320. " +
+                "This IS LOONG! 12343214098239048092314809823140982309840 012398409 42309 01932840 918804 l 023984098109230984032981 this is a testhello" +
+                "The END!.!.!.!.! HA lol!";
+            var resultString = Cryptography.Encrypt(value, "myPass");
+            var decriptedResultString = Cryptography.Decrypt(resultString, "myPass");
+            Assert.Equal(value, decriptedResultString);
+        }
+
+        [Theory]
+        [InlineData("my super Secret Data", "my Dull password")]
+        [InlineData("MORE super Secret Data!", "P@S$woRD!^")]
+        [InlineData("awes0me DATa", "0000001230000000")]
+        public void Cryptography_aes_encrypt_and_decrypt_match_Theory_Pass(string value, string password)
+        {
+            var resultString = Cryptography.Encrypt(value, password);
+            var decriptedResultString = Cryptography.Decrypt(resultString, password);
+            Assert.Equal(value, decriptedResultString);
+        }
+
+        [Fact]
+        public void Cryptography_aes_encrypt_and_decrypt_bad_pass_Fail()
+        {
+            var resultString = Cryptography.Encrypt("my secret", "myPass");
+            var decriptedResultString = Cryptography.Decrypt(resultString, "myPassBogus");
+            Assert.NotEqual("my secret", decriptedResultString);
+        }
+
+        [Theory]
+        [InlineData("my super Secret Data", "my Dull password")]
+        [InlineData("MORE super Secret Data!", "P@S$woRD!^")]
+        [InlineData("awes0me DATa", "0000001230000000")]
+        public void Cryptography_aes_encrypt_and_decrypt_bad_pass_Theory_Fail(string value, string password)
+        {
+            var resultString = Cryptography.Encrypt(value, password);
+            var bogusPw = password + "BOGUS";
+            var decriptedResultString = Cryptography.Decrypt(resultString, bogusPw);
+            Assert.NotEqual(value, decriptedResultString);
+        }
+
+        #endregion
+    }
+}
